18c544f1fabc67eee4c38280798918641949494a
pacholoamit <pacholoamit.tech@gmail.com>
2022-02-26 07:33:29 +0800

    minor changes in layout

--- lib/web/src/App.tsx
+++ lib/web/src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
 import { Navbar, Hero, Exchange, Footer } from "./components";
+import { Center } from "@mantine/core";
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <Navbar />
-      <Hero />
-      <Exchange />
-      <Footer />
-    </div>
+    <Center>
+      <div className="App">
+        <Navbar />
+        <Hero />
+        <Exchange />
+        <Footer />
+      </div>
+    </Center>
   );
 };
 
