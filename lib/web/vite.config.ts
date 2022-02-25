d5414f6e1bb0aff34eec5cc786c37be76fd6bd82
pacholoamit <pacholoamit.tech@gmail.com>
2022-02-25 17:06:50 +0800

    remove react refresh

--- lib/web/vite.config.ts
+++ lib/web/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,5 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3000,
   },
-  plugins: [reactRefresh()],
+  plugins: [react()],
 });
