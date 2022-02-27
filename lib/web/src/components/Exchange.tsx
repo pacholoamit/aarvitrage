15ebd7201815a5ef9889684e183e48f06a95d9c0
pacholoamit <pacholoamit.tech@gmail.com>
2022-02-27 17:47:04 +0800

    connection completed from web to api

--- lib/web/src/components/Exchange.tsx
+++ lib/web/src/components/Exchange.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Autocomplete, Group } from "@mantine/core";
+import axios from "axios";
+import { Autocomplete, Button, Group } from "@mantine/core";
 
 const Exchange: React.FC = () => {
+  const runQuery = async () => {
+    const result = await axios.get(
+      "https://aarvitrage-api.arch-server.com/app"
+    );
+    console.log(result);
+  };
   return (
     <Group position="center" direction="column">
       <Autocomplete
@@ -9,6 +16,7 @@ const Exchange: React.FC = () => {
         placeholder="BTC/USDT"
         data={["React", "Angular", "Svelte", "Vue"]}
       />
+      <Button onClick={runQuery}> Test query</Button>
     </Group>
   );
 };
