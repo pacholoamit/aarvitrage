3350417dde490e0af145a37a3525b6a03c2860b9
pacholoamit <pacholoamit.tech@gmail.com>
2022-03-05 18:34:58 +0800

    delete deprecated code

--- lib/web/vite.config.ts
+++ lib/web/vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-    https: false,
-    hmr: {
-      host: "aarvitrage.arch-server.com",
-      port: 443,
-      protocol: "wss",
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+
+    server: {
+      port: 3000,
+      https: false,
+      hmr: {
+        host: process.env.DOMAIN_URL ?? "localhost",
+        port: 443,
+        protocol: "wss",
+      },
     },
-  },
-  plugins: [react()],
-});
+  });
+};
